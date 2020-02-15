ddd6e065192f1ac16ce4190740300cb350cb6fe8
chrispforlife <60669967+chrispforlife@users.noreply.github.com>
2020-02-15 16:45:45 -0500

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include "Customer.h"
+using namespace std;
 
+//Constructors
+Customer::Customer(string cnu, string cna, double l)
+{
+    customerNum=cnu;
+    customerName=cna;
+    lineOfCredit=l;
+}
+
+Customer::Customer(){}
+
+//Get Functions
+string Customer::getcustomerNum(){return customerNum;}
+string Customer::getcustomerName(){return customerName;}
+double Customer::getlineOfCredit(){return lineOfCredit;}
+
+//Print Function
+string Customer::getCustomer()
+{
+    
+}
+
+//Set Functions
+void Customer::setcustomerNum(string cnu){customerNum=cnu;}
+void Customer::setcustomerName(string cna){customerName=cna;}
+void Customer::setlineOfCredit(double l){lineOfCredit=l;}
