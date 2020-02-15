26d2a621a1bab185760a50991dab11bae0bf330d
chrispforlife <60669967+chrispforlife@users.noreply.github.com>
2020-02-15 16:48:17 -0500

    Update Product.h

--- Product.h
+++ Product.h
@@ -1,1 +1,29 @@
+#include <string>
 
+class Product
+{
+    int itemNo;
+    std::string description;
+    double price;
+    int stockQuantity;
+    
+    public:
+    Product(int, std::string, double, int);
+    Product();
+    
+    //Get Functions
+    int getitemNo();
+    std::string getdescription();
+    double getprice();
+    int getstockQuantity();
+    
+    //Print Function
+    void getProduct();
+    
+    //Set Functions
+    void setitemNo(int);
+    void setdescription(std::string);
+    void setprice(double);
+    void setstockQuantity(int);
+    
+};
