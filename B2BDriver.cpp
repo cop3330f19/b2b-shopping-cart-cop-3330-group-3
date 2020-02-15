2e7c8384d09f96d57f39f2bff1d421954100991a
TheGreatPerhaps <61095672+TheGreatPerhaps@users.noreply.github.com>
2020-02-15 18:16:26 -0500

    Update B2BDriver.cpp

--- B2BDriver.cpp
+++ B2BDriver.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include "Customer.h"
+#include "Address.h"
+#include "Product.h"
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+cout << "--------------------------------------------------" << endl;
+cout << "B2B Shopping Cart << endl;
+cout << "--------------------------------------------------" << endl << endl;  
+  
+cout << "Order Number: " <<   << endl;
+cout << "Associate: " <<   << endl;
+cout << "Customer Number: " <<   << endl;
+cout << "Customer: " <<   << endl;
+cout << "Address: " <<   << endl << endl;
+
+cout << "--------------------------------------------------" << endl;
+cout << "Item No      Description           Qty     Total" << endl;
+cout << "--------------------------------------------------" << endl; 
+cout <<  << "      " <<  << "           " <<  << "     " <<  << endl; 
+cout <<  << "      " <<  << "           " <<  << "     " <<  << endl; 
+cout <<  << "      " <<  << "           " <<  << "     " <<  << endl; 
+cout <<  << "      " <<  << "           " <<  << "     " <<  << endl;   
+cout << "--------------------------------------------------" << endl;
+cout << "Total" <<  << endl;
+cout << "--------------------------------------------------" << endl;  
+cout << "Remaining Credit" <<  << endl;  
+  
+  
+  
+ 
+     
+   
+  
+  
+  return 0;
+} 
 
