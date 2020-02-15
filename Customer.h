646292e997dcdf293839ef537b8dfd645445bb2e
chrispforlife <60669967+chrispforlife@users.noreply.github.com>
2020-02-15 16:46:40 -0500

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -1,1 +1,27 @@
+#include <string>
+#include "Address.h"
 
+ class Customer
+ {
+     std::string customerNum;
+     std::string customerName;
+     double lineOfCredit;
+     Address * corperateAddress;
+     
+     public:
+     Customer(std::string, std::string, double);
+     Customer();
+     
+     //Get Functions
+     std::string getcustomerNum();
+     std::string getcustomerName();
+     double getlineOfCredit();
+     
+     //Print Function
+     std::string getCustomer();
+     
+     //Set Functions
+     void setcustomerNum(std::string);
+     void setcustomerName(std::string);
+     void setlineOfCredit(double);
+ };
