0b67b10a7f0e374cca059ade09a1d84b995051fc
chrispforlife <60669967+chrispforlife@users.noreply.github.com>
2020-02-15 16:39:17 -0500

    Update Address.h

--- Address.h
+++ Address.h
@@ -1,1 +1,30 @@
+#include <string>
 
+class Address
+{
+  //data members
+  std::string streetAddress;
+  std::string city;
+  std::string state;
+  std::string zipCode;
+  
+  public:
+  //constructors
+  Address(std::string, std::string, std::string, std::string);
+  Address();
+  
+  //Get Functions
+  std::string getstreetAddress();
+  std::string getcity();
+  std::string getstate();
+  std::string getzipCode();
+  
+  //Print function
+  std::string getAddress();
+  
+  //Set Functions
+  void setstreetAddress(std::string);
+  void setcity(std::string);
+  void setstate(std::string);
+  void setzipCode(std::string);
+};
