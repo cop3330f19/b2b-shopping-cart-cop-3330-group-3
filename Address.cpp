dadc922759e86838433c6da86b8f192d15e1786d
chrispforlife <60669967+chrispforlife@users.noreply.github.com>
2020-02-15 16:43:51 -0500

    Update Address.cpp

--- Address.cpp
+++ Address.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include "Address.h"
+using namespace std;
 
+//constructors given definition
+Address::Address(string sa, string c, string s, string z)
+{
+    streetAddress=sa;
+    city=c;
+    state=s;
+    zipCode=z;
+} 
+Address::Address(){}
+
+//Get Functions
+string Address::getstreetAddress(){return streetAddress;}
+string Address::getcity(){return city;}
+string Address::getstate(){return state;}
+string Address::getzipCode(){return zipCode;}
+
+//Print Function
+string Address::getAddress()
+{
+    return (streetAddress+","+city+","+state+","+zipCode); 
+}
+
+//Set Functions
+void Address::setstreetAddress(string sa){streetAddress=sa;}
+void Address::setcity(string c){city=c;}
+void Address::setstate(string s){state=s;}
+void Address::setzipCode(string z){zipCode=z;}
