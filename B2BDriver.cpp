a1b520f8af4a63053bb724b3e46c124be03f874a
TheGreatPerhaps <61095672+TheGreatPerhaps@users.noreply.github.com>
2020-02-15 19:33:49 -0500

    Update B2BDriver.cpp

--- B2BDriver.cpp
+++ B2BDriver.cpp
@@ -9,10 +9,37 @@ using namespace std;
 
 int main()
 {
-  
 ifstream myIn ("customers.dat");
 ifstream myIn2 ("inventory.dat");  
-ofstream myOut ("abc");      
+ofstream myOut ("dontknowyet");  //Dont Know What This File is supposed to be yet 
+  
+//Variables for Customer
+string cName, cNum;
+double loc; 
+  
+//Variables for Product
+int iNum, sQuant;
+string descript;
+double pri;
+
+//Created object of Customer class  
+Customer customer;
+  
+  
+myIn >> cName >> cNum >> loc;
+  
+customer.setcustomerNum(cName);
+customer.setcustomerName(cName); 
+customer.setlineOfCredit(loc);
+  
+myIn2 >> iNum >> descript >> pri >> sQuant;
+  
+customer.setitemNo(iNum);
+customer.setdescription(descript); 
+customer.setprice(pri);
+customer.setstockQuantity(sQuant);
+  
+  
 
 cout << "--------------------------------------------------" << endl;
 cout << "B2B Shopping Cart << endl;
