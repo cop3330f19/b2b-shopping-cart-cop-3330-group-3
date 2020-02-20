59306a29eeaaae704e61a85172a06a905613cd33
chrispforlife <60669967+chrispforlife@users.noreply.github.com>
2020-02-20 18:26:51 -0500

    Update Product.h

--- Product.h
+++ Product.h
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 
 class Product
 {
@@ -11,19 +12,23 @@ class Product
     Product(int, std::string, double, int);
     Product();
     
+    std::vector<Product> product;
+    
     //Get Functions
     int getitemNo();
     std::string getdescription();
     double getprice();
     int getstockQuantity();
     
+    //Recieve from file Function
+    void grabinfo();
+    
     //Print Function
-    void getProduct();
+    std::string getProduct();
     
     //Set Functions
     void setitemNo(int);
     void setdescription(std::string);
     void setprice(double);
     void setstockQuantity(int);
-    
 };
