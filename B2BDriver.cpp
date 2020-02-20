0db955c46cc3c8ab86fccbdc830088e070138916
chrispforlife <60669967+chrispforlife@users.noreply.github.com>
2020-02-20 18:32:25 -0500

    Update B2BDriver.cpp

--- B2BDriver.cpp
+++ B2BDriver.cpp
@@ -1,75 +1,60 @@
-#include <iostream>
+#include <ctime>
+#include<sstream>
 #include <fstream>
+#include <iostream>
 #include "Customer.h"
-#include "Address.h"
 #include "Product.h"
-#include <string>
-
+#include <vector>
 using namespace std;
 
-int main()
+string generateOrderNum()
 {
-ifstream myIn ("customers.dat");
-ifstream myIn2 ("inventory.dat");  
-ofstream myOut ("dontknowyet");  //Dont Know What This File is supposed to be yet 
-  
-//Variables for Customer
-string cName, cNum;
-double loc; 
-  
-//Variables for Product
-int iNum, sQuant;
-string descript;
-double pri;
+    time_t now = time(0);
+    string oNum;
+    stringstream strstream;
+    strstream << now;
+    strstream >> oNum;
+    return oNum;    
+}
 
-//Created object of Customer class  
-Customer customer;
-  
-  
-myIn >> cName >> cNum >> loc;
-  
-customer.setcustomerNum(cName);
-customer.setcustomerName(cName); 
-customer.setlineOfCredit(loc);
-  
-myIn2 >> iNum >> descript >> pri >> sQuant;
-  
-customer.setitemNo(iNum);
-customer.setdescription(descript); 
-customer.setprice(pri);
-customer.setstockQuantity(sQuant);
-  
-  
 
-cout << "--------------------------------------------------" << endl;
-cout << "B2B Shopping Cart << endl;
-cout << "--------------------------------------------------" << endl << endl;  
-  
-cout << "Order Number: " <<   << endl;
-cout << "Associate: " <<   << endl;
-cout << "Customer Number: " <<   << endl;
-cout << "Customer: " <<   << endl;
-cout << "Address: " <<   << endl << endl;
-
-cout << "--------------------------------------------------" << endl;
-cout << "Item No      Description           Qty     Total" << endl;
-cout << "--------------------------------------------------" << endl; 
-cout <<  << "      " <<  << "           " <<  << "     " <<  << endl; 
-cout <<  << "      " <<  << "           " <<  << "     " <<  << endl; 
-cout <<  << "      " <<  << "           " <<  << "     " <<  << endl; 
-cout <<  << "      " <<  << "           " <<  << "     " <<  << endl;   
-cout << "--------------------------------------------------" << endl;
-cout << "Total" <<  << endl;
-cout << "--------------------------------------------------" << endl;  
-cout << "Remaining Credit" <<  << endl;  
-  
-  
-  
- 
-     
-   
-  
-  
-  return 0;
-} 
-
+int main()
+{ 
+    Address a;
+    Customer c;
+    Product p;
+    
+    p.grabinfo();
+    vector<Product> prod=p.product;
+    
+    c.grabcustomer();
+    vector <Customer> cust=c.cust;
+    
+    vector <Address> addresses
+    
+    for(int i=0; i<cust.size(); i++)
+    {
+        cout << cust[i].getcustomerName() << endl;
+    } 
+    for (int i=0; i<prod.size(); i++)
+    {
+       if(prod[i].getcustomerName()==cusname)
+    }
+    
+    //associate enters name 
+    //associate looks up the customer
+    //associate enters selected items/quantities
+    
+    /*string asc;
+    cout << "Who are you(Associate Name)? ";
+    cin >> asc;*/
+    
+    
+    /*
+    cout << "Order Number: " << generateOrderNum()<< endl;
+    cout << "Associate: " << asc << endl; 
+    cout << "Customer Number: " << endl;
+    cout << "Costumer: " << endl;
+    cout << "Address: " << endl; */
+	return 0;
+}
