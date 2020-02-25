883b5e635fba0574cac3453db4ad18a58c214c54
hunterb3 <61298098+hunterb3@users.noreply.github.com>
2020-02-25 14:41:25 -0500

    Update B2BDriver.cpp

--- B2BDriver.cpp
+++ B2BDriver.cpp
@@ -2,7 +2,7 @@
 B2BDriver.cpp
 Group 3 Members: Hunter Bell, Tariq Green, Darrell Brown, Christian Pierre Paul
 Date Last Edited: 02/25/20
-Purpose:
+Purpose: To test our classes in the program.
 */
 
 #include <ctime>
