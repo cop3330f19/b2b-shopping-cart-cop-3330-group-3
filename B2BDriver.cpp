b21c28e7f2ce156cb40c7a7cdf7ef653ac4ad9ce
Darrell1017 <57148454+Darrell1017@users.noreply.github.com>
2020-02-25 00:15:42 -0500

    Update B2BDriver.cpp

--- B2BDriver.cpp
+++ B2BDriver.cpp
@@ -5,6 +5,7 @@
 #include "Customer.h"
 #include "Product.h"
 #include <vector>
+#include <string>
 using namespace std;
 
 string generateOrderNum()
@@ -14,112 +15,48 @@ string generateOrderNum()
     stringstream strstream;
     strstream << now;
     strstream >> oNum;
-    return oNum;
+    return oNum;    
 }
-
 
 int main()
 {
-    //Variables for Customer
-    string cName, cNum;
-    double loc;
-
-    //Variables for Product
-    int iNum, sQuant;
-    string descript;
-    double pri;
-
+    //creating objects to access classes
     Address a;
     Customer c;
     Product p;
-
+    //runs the function in the class to store objects of information from each file
     p.grabinfo();
-    vector<Product> prod = p.product;
-
     c.grabcustomer();
-    vector <Customer> cust = c.cust;
-
-    vector <Address> addresses;
-
-        for (int i = 0; i < cust.size(); i++)
-        {
-            cout << cust[i].getcustomerName() << endl;
-        }
-    for (int i = 0; i < prod.size(); i++)
-    {
-        if (prod[i].getcustomerName() == cName)
-    }
-
+    //the vector containing the objects that store the files from each file
+    vector<Product> prod=p.product;
+    vector <Customer> custo=c.cust;
+    vector <Address> add=c.addy;
+    vector <Product> transact;
    
-    //associate enters name 
-    string asc;
 
-    if (asc == customer)
-    {
-        cout << "AssociateName :" << endl;
-    }
-    else
-        return ;
-    //associate looks up the customer
-    if (customer == cName)
-    else
-        return ;
-    //associate enters selected items/quantities
-    if (sQuant == iNum)
-    {
-        for (int = i; i = 0; i--)
-    }
-    else
-        return ;
-
-    string asc;
+    string asc, cus; //variable for associate and customer to enter their name/the name
+    int itnu, quan; //the item number and quantity of the item used in the transaction
+    int indcus, indpro; //holds the values of the index of the product and customer;
+   
+    //associate enters name
     cout << "Who are you(Associate Name)? ";
     cin >> asc;
-
-      //associate enters selected items
-    double total=0;
-    flag=false;
-    while(true)
+   
+    //associate looks up the customer
+    bool flag=false;
+    while (true)
     {
-       cout << "What item # of the item that was used in the transaction?(Enter 0000 to STOP) ";
-       cin >> itnu;    
-        for (int i=0; i<prod.size(); i++)
+        cout << "Who is the customer of this transacction? ";
+        cin >> cus;
+        for (int i=0; i<custo.size(); i++)
         {
-            if (itnu==0000)
-            {
-                flag=true;
-            }
-            if(itnu==prod[i].getitemNo()) //verifies if the item number exists in the file
-            {
-                cout << "What was the quantity purchased? ";
-                cin >> quan;
-                if (quan<=prod[i].getstockQuantity()) //verifies if there is enough of the item to satisfy the quantity
-                {
-                    indpro=i;
-                    total+=(quan*prod[i].getprice()); //updates the total price of transaction
-                    prod[i].setstockQuantity(prod[i].getstockQuantity()-quan); //updates stock during transaction
-                    custo[indcus].setlineOfCredit(custo[indcus].getlineOfCredit()-(quan*prod[i].getprice())); //updates the customer's line of credit
-                    Product tr(prod[i].getitemNo(), prod[i].getdescription(), prod[i].getprice(), prod[i].getstockQuantity());
-                    transact.push_back(tr);
-                    break;
-                } 
-                else 
-                {
-                    cout << "Sorry! Re-enter available item and quantity " <<endl;
-                }
-            }
-            //cout << "The final total is " <<prod[i].getitemNo() << " is " << total <<endl;
+          if (cus==custo[i].getcustomerName())
+          {
+              indcus=i;
+              flag=true;
+              break;
+          }
         }
         if (flag==true)
             break;
     }
-    
-    cout << "Order Number: " << generateOrderNum()<< endl;
-    cout << "Associate: " << asc << endl;
-    cout << "Customer Number: " << endl;
-    cout << "Costumer: " << endl;
-    cout << "Address: " << endl; 
-    return 0;
-}
-
-
